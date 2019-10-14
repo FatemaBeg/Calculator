ef1adb1f29bca6e89231e0aea56be8bfe4db96e9
FatemaBeg <fatema007beg@gmail.com>
2019-10-14 22:20:00 +0600

    calculation

---
+++ CalculatorAppPractice2/CalculatorUi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorAppPractice2;
+
+namespace CalculatorAppPractice2
+{
+    public partial class CalculatorUi : Form
+    {
+        Result _result = new Result();
+        
+        public CalculatorUi()
+        {
+            InitializeComponent();
+        }
+        
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+           
+            _result.FirstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            _result.SecondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+           
+            resultTextBox.Text = _result.add().ToString();
+        }
+
+        private void SubtractButton_Click(object sender, EventArgs e)
+        {
+            _result.FirstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            _result.SecondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            resultTextBox.Text = _result.Subtract().ToString();
+        }
+
+        private void MultiplyButton_Click(object sender, EventArgs e)
+        {
+            _result.FirstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            _result.SecondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            resultTextBox.Text = _result.Multiply().ToString();
+        }
+
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+            _result.FirstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            _result.SecondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            resultTextBox.Text = _result.Divide().ToString();
+        }
+
+        private void resultTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ CalculatorAppPractice2/Result.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAppPractice2
+{
+   public  class Result
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public double CalculatedResult { get; set; }
+
+        public double add()
+        {
+           CalculatedResult= FirstNumber + SecondNumber;
+            return CalculatedResult;
+        }
+        public double Subtract()
+        {
+            CalculatedResult = FirstNumber - SecondNumber;
+            return CalculatedResult;
+        }
+        public double Multiply()
+        {
+            CalculatedResult = FirstNumber * SecondNumber;
+            return CalculatedResult;
+        }
+        public double Divide()
+        {
+            CalculatedResult = FirstNumber / SecondNumber;
+            return CalculatedResult;
+        }
+
+
+
+    }
+}
